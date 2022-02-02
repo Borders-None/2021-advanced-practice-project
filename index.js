69fa43a0d808695fd9098d9ecbf8b03cf79c36d9
Fatma <shauqifatma@gmail.com>
2022-02-02 17:30:52 +0100

    appended childs by className

--- index.js
+++ index.js
@@ -3,27 +3,49 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 async function main() {
-  let response = await fetch(" http://localhost:3000apiemails");
+  let response = await fetch("http://localhost:3000/api/emails");
   let data = await response.json();
   console.log(data);
 
-  for (let i = 0; i < 10; i++) {
-    console.log(i);
-    return data;
+  for (let i = 0; i < data.emails.length; i++) {
+    var email = data.emails[i];
+
+    var emaiList = document.getElementById("email-list");
+    var emailElement = document.createElement("div");
+    emailElement.className = "emails";
+    emaiList.appendChild(emailElement);
+
+    //append emailrow
+
+    var emailRow = document.createElement("div");
+    emailRow.className = "row";
+    emailElement.appendChild(emailRow);
+
+    //append options
+
+    var emailOption = document.createElement("div");
+    emailOption.className = "options";
+    emailRow.appendChild(emailOption);
+    emailOption.innerHTML =
+      "<input type='checkbox'><i class='fa fa-star-o' aria-hidden='true'></i><span class='material-icons'>label_important</span>";
+
+    //append h3 title
+
+    var rowTitel = document.createElement("h3");
+    rowTitel.className = "row-titel";
+    emailOption.appendChild(rowTitel);
+
+    //append message div
+
+    var rowMessage = document.createElement("div");
+    rowMessage.className = "row-message";
+    rowTitel.appendChild(rowMessage);
+
+    //append h4
+    var textMessage = document.createElement("h4");
+    textMessage.className = "message";
+    rowMessage.appendChild(textMessage);
+
+    console.log(email);
   }
-  const showEmail = (document.querySelector(".emailrow").innerHTML =
-    "<h1>" +
-    data[1].id +
-    "</h1>" +
-    +"<h3>" +
-    data[1].from +
-    "</h3>" +
-    "<h1>" +
-    data[1].title +
-    "</h1>" +
-    "<h1>" +
-    data[1].date +
-    "</h1>");
-  showEmail.addEventListener("click", main);
 }
-console.log(main());
