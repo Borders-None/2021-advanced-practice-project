901b898f220ad6835597f71b421c1606b2979265
odda1991 <96075423+odda1991@users.noreply.github.com>
2022-02-03 21:30:21 +0100

    fetch request

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ async function main() {
   console.log("Hello world!");
 
   for (let i = 0; i < 10; i++) {
-    console(i);
+    console.log(i);
   }
 
   var number = 5;
@@ -17,4 +17,46 @@ async function main() {
 
   number = number * 10;
   number += 10;
+};
+var url = "http://localhost:3000/api/emails" ;
+fetch(url)
+  .then(Response => Response.json())
+  .then(data => getMails(data));
+
+function getMails(data){
+  console.log(data);
+  for(let i=0; i < data.emails.length ; i++){
+   var element = document.createElement('tr');
+   var box = document.getElementById('mailbox');
+   box.appendChild(element);
+   element.classList.add('email_row');
+
+   var choice = document.createElement('input');
+   choice.setAttribute("type" , "checkbox");
+   element.appendChild(choice);
+
+   var nameSender = document.createElement('td');
+   element.appendChild(nameSender);
+   nameSender.innerText = data.emails[i].from;
+
+   var title = document.createElement('td');
+   element.appendChild(title);
+   title.innerText = data.emails[i].title;
+
+   var time = document.createElement('td');
+   element.appendChild(time);
+   var date = new Date(data.emails[i].date);
+   var dateString = date.toLocaleDateString();
+   time.innerText = dateString;
+  }
+
 }
+
+
+/*getMails("http://localhost:3000/api/emails") ;*/
+
+/*async function getMails(url) {
+  let request = await fetch(url);
+  let Response = await request.emails();
+  document.getElementById(). innerHTML = Response;
+}*/
