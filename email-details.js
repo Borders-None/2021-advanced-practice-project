4ca4d12a6278b496e4cbd33095903eb36d0abb75
Fatma <shauqifatma@gmail.com>
2022-03-06 17:34:08 +0100

    change the email details layout

--- email-details.js
+++ email-details.js
@@ -1,57 +1,57 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const emailId = urlParams.get("id");
+  const urlParams = new URLSearchParams(window.location.search);
+  const emailId = urlParams.get("id");
 
-    var wait = document.getElementById('mail_content');
-    var  letterWait = document.createElement('h2');
-    wait.appendChild(letterWait);
-    letterWait.innerText= "Loading...";
+  var wait = document.getElementById("mail_content");
+  var letterWait = document.createElement("h2");
+  wait.appendChild(letterWait);
+  letterWait.innerText = "Loading...";
 
-    var url = "http://localhost:3000/api/emails/" + emailId;
-    fetch(url)
-        .then(Response => Response.json())
-        .then(data => {
-            wait.removeChild(letterWait);
-            showDetails(data)
-        });
-      
-    function showDetails(data){
-        console.log(data);
-        var mainbox = document.getElementById('mail_content');
-        var sender = document.createElement('div');
-        mainbox.appendChild(sender);
-        sender.innerText = "From:" + " "  + data.from;
+  var url = "http://localhost:3000/api/emails/" + emailId;
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+      wait.removeChild(letterWait);
+      showDetails(data);
+    });
 
-        var title = document.createElement('div')
-        mainbox.appendChild(title);
-        title.innerText = "Subject:" + " " + data.title;
+  function showDetails(data) {
+    console.log(data);
+    var mainbox = document.getElementById("mail_content");
+    var sender = document.createElement("div");
+    sender.classList.add("first");
+    mainbox.appendChild(sender);
+    sender.innerText = "From:" + " " + data.from;
 
-        var time = document.createElement('div');
-        mainbox.appendChild(time);
-        var date =  new Date (data.date);
-        var dateString = date.toLocaleDateString();
-        time.innerText = dateString;
+    var title = document.createElement("div");
+    title.classList.add("first");
+    mainbox.appendChild(title);
+    title.innerText = "Subject:" + " " + data.title;
 
-        var body = document.createElement('div');
-        mainbox.appendChild(body);
-        body.innerText = data.body;
+    var time = document.createElement("div");
+    time.classList.add("first");
+    mainbox.appendChild(time);
+    var date = new Date(data.date);
+    var dateString = date.toLocaleDateString();
+    time.innerText = dateString;
 
-        var opition = document.createElement('div');
-        mainbox.appendChild(opition);
-        opition.classList.add('footer');
+    var body = document.createElement("div");
+    body.classList.add("first");
+    mainbox.appendChild(body);
+    body.innerText = data.body;
 
+    var opition = document.createElement("div");
+    mainbox.appendChild(opition);
+    opition.classList.add("footer");
 
-        var replay = document.createElement('button');
-        opition.appendChild(replay);
-        replay.classList.add('replay');
-        replay.innerText = "Replay";
+    var replay = document.createElement("button");
+    opition.appendChild(replay);
+    replay.classList.add("replay");
+    replay.innerText = "Replay";
 
-
-        var forward = document.createElement('button');
-        opition.appendChild(forward);
-        forward.classList.add('replay');
-        forward.innerText = "forward";
-        
-    }
-  })
-    
+    var forward = document.createElement("button");
+    opition.appendChild(forward);
+    forward.classList.add("replay");
+    forward.innerText = "forward";
+  }
+});
